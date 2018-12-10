4e028bc8e29f2ead346e4e7fe8ac53848f494c1a
Lucas <lucashen@gmail.com>
2018-12-10 16:21:46 -0300

    add component certificado

---
+++ src/app/components/certificado/certificado.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-certificado',
+  templateUrl: './certificado.component.html',
+  styleUrls: ['./certificado.component.css']
+})
+export class CertificadoComponent implements OnInit {
+
+  public form = {
+    email: null
+  };
+
+  public error = null;
+  loading = false;
+
+  constructor(private User: UserService) { }
+
+  onSubmit() {
+    this.loading = true;
+    this.User.certificado(this.form).subscribe(
+      data => {
+        this.loading = false;
+        this.error = '';
+        window.open('http://127.0.0.1:8000/files/certificados/' + data['data'], '_blank');
+      },
+      error => {
+        this.loading = false;
+        this.error = error.error.error;
+      }
+    );
+  }
+
+  ngOnInit() {
+  }
+
+}
